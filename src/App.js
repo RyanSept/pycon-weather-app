aee6605dd0371b855d12efe490eaa9c4826068fb
Ryan Marvin <marvinryan@ymail.com>
2018-10-17 03:19:09 +0300

    Add icons and stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from 'material-ui/TextField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { grey50, pink300 } from 'material-ui/styles/colors';
+import { getWeather } from './api.js';
 
 class App extends Component {
+  componentDidUpdate(prevProps) {
+    this.setState()
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <MuiThemeProvider >
+        <div className="App">
+          <header className="App-header">
+            <i className="weather-icon owf owf-522 owf-5x"></i>
+            <TextField
+              floatingLabelText="Type in the city and hit enter"
+              defaultValue="Nairobi"
+              floatingLabelStyle={{ color: pink300 }}
+              inputStyle={{ color: pink300 }}
+              style={{ color: "white" }}
+              // fullWidth={true}
+            />
+          </header>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/api.js
@@ -0,0 +1,14 @@
+export default function getWeather(city) {
+    return {
+        "id": 2643743,
+        "name": "Nairobi",
+        "temp": 7,
+        "weather": {
+            "id": 701,
+            "main": "Mist",
+            "description": "mist",
+            "icon": "50d"
+        },
+
+    };
+}
