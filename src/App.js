75c207baef2a158d90b41f56f7367f158faa8688
Ryan Marvin <marvinryan@ymail.com>
2018-10-19 02:59:17 +0300

    Fix styling issues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import TextField from '@material-ui/core/TextField';
-import { pink } from '@material-ui/core/colors';
+import TextField from 'material-ui/TextField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { grey50, pink200, pink300 } from 'material-ui/styles/colors';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import Icon from '@material-ui/core/Icon';
@@ -34,40 +35,45 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Grid container className="App-header">
-          <i className={`weather-icon owf ${this.state.icon}`}></i>
-          <Grid item xs={12} className="weather-details">
-            <p>{this.state.temperature}ºC</p>
-            <p>{this.state.location}</p>
-            <p>{this.state.weather_state}</p>
-          </Grid>
-          <Grid item xs={12}>
-            <Grid container spacing={16}>
-              <Grid item xs={11}>
-                <TextField
-                  label="Type in the city and hit enter"
-                  defaultValue={this.state.location}
-                  style={{ color: "white" }}
-                  onChange={this.handleTextInputChange}
-                  onKeyPress={(ev) => {
-                    if (ev.key === 'Enter' && ev.target.value !== "") {
-                      this.fetchWeather(ev.target.value);
-                      ev.preventDefault();
-                    }
-                  }}
-                // fullWidth={true}
-                />
-              </Grid>
-              <Grid item xs={1}>
-                <IconButton color="secondary" aria-label="Add an alarm">
-                  <Icon>🔔</Icon>
-                </IconButton>
+      <MuiThemeProvider >
+        <div className="App">
+          <Grid container className="App-header">
+            <i className={`weather-icon owf ${this.state.icon}`}></i>
+            <Grid item xs={12} className="weather-details">
+              <p>{this.state.temperature}ºC</p>
+              <p>{this.state.location}</p>
+              <p>{this.state.weather_state}</p>
+            </Grid>
+            <Grid item xs={12}>
+              <Grid container spacing={4}>
+                <Grid item xs={10}>
+                  <TextField
+                    floatingLabelText="Type in the city and hit enter"
+                    defaultValue={this.state.location}
+                    floatingLabelStyle={{ color: pink300 }}
+                    underlineFocusStyle={{ borderColor: pink200 }}
+                    inputStyle={{ color: pink300 }}
+                    style={{ color: "white" }}
+                    onChange={this.handleTextInputChange}
+                    onKeyPress={(ev) => {
+                      if (ev.key === 'Enter' && ev.target.value !== "") {
+                        this.fetchWeather(ev.target.value);
+                        ev.preventDefault();
+                      }
+                    }}
+                  // fullWidth={true}
+                  />
+                </Grid>
+                <Grid item xs={2}>
+                  <IconButton color="secondary" aria-label="Add an alarm">
+                    <Icon>notifications</Icon>
+                  </IconButton>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
